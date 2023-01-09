83a6e6ba462f09b4471a2dcf5961b94321496fc9
muac404 <muac404@gmail.com>
2023-01-09 15:46:31 +0100

    first commit

---
+++ index.php
@@ -0,0 +1,24 @@
+<?php
+class Movie {
+    public $titolo;
+    public $durata;
+    public $rating;
+}
+$avatar = new Movie();
+$avatar->titolo = "Avatar";
+$avatar->durata = "118";
+$avatar->rating = "7";
+
+$elCamino = new Movie();
+$elCamino->titolo = "El Camino";
+$elCamino->durata = "89";
+$elCamino->rating = "6";
+
+$rumoreBianco = new Movie();
+$rumoreBianco->titolo = "Rumore Bianco";
+$rumoreBianco->durata = "99";
+$rumoreBianco->rating = "4";
+
+
+
+?>
