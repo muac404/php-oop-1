cdd0c92b9acedb26eaffa72243509727710a8d0d
muac404 <muac404@gmail.com>
2023-01-09 16:05:07 +0100

    Aggiunge construct $titolo e $durata

--- index.php
+++ index.php
@@ -3,20 +3,26 @@ class Movie {
     public $titolo;
     public $durata;
     public $rating;
+    function __construct($titolo, $durata) {
+    $this->titolo = $titolo; 
+    $this->durata = $durata; 
+  }
+  function get_titolo() {
+    return $this->titolo;
+  }
+  function get_durata() {
+    return $this->durata;
+  }
+
+    
 }
-$avatar = new Movie();
-$avatar->titolo = "Avatar";
-$avatar->durata = "118";
+$avatar = new Movie("Avatar","118");
 $avatar->rating = "7";
 
-$elCamino = new Movie();
-$elCamino->titolo = "El Camino";
-$elCamino->durata = "89";
+$elCamino = new Movie("El Camino", "89");
 $elCamino->rating = "6";
 
-$rumoreBianco = new Movie();
-$rumoreBianco->titolo = "Rumore Bianco";
-$rumoreBianco->durata = "99";
+$rumoreBianco = new Movie("Rumore Bianco", "99");
 $rumoreBianco->rating = "4";
 
 
