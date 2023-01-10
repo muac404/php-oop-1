53cde251154f90647b1fe05c35b0d50b28532661
muac404 <muac404@gmail.com>
2023-01-10 16:45:12 +0100

    checkpoint

--- index.php
+++ index.php
@@ -28,3 +28,17 @@ $rumoreBianco->rating = "4";
 
 
 ?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>PHP OOP 1</title>
+</head>
+<body>
+    <p><?php echo $avatar; ?></p>
+    
+</body>
+</html>
